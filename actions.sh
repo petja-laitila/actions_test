86c808c64c2785e59ae5860b974593d749d1ccc1
petja-laitila <petja.laitila@otavanopisto.fi>
2020-12-18 11:01:16 +0200

    furthering along

--- actions.sh
+++ actions.sh
@@ -7,17 +7,20 @@ if [[ ($commitmessage == *"Merge pull request"*) && ($commitmessage == *"from pe
     # git remote set-url origin git@github.com:petja-laitila/actions_test.git
     git config user.name "Github Actions Bot"
     git config user.email "github-actions[bot]@users.noreply.github.com"
-    # git config --global push.default simple
-    # git checkout master
-    # git reset --hard
-    # git pull
+    git config pull.rebase false
+    # Show me remotes
+    git remote -v
+    # Push one branch at a time
+    git config --global push.default simple
+    git checkout master
+    git reset --hard
+    git pull
     echo $commitmessage | tee -a test
     git add .
     git commit -m "Updated some stuff here"
     git push
     echo Merging changes back to devel
     # git fetch
-    git pull
     git checkout devel
     git pull
     git merge master
