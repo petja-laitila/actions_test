28ca79f4edc8363b1401c6d2e65696cf1cef8f15
Petja Laitila <petja.laitila@otavanopisto.fi>
2026-09-10 11:51:12 +0300

    Update actions.sh

--- actions.sh
+++ actions.sh
@@ -1,40 +1,23 @@
 #!/bin/bash
-commitmessage=`git log --pretty=format:"%s" -1`;
-if [[ ($commitmessage == *"Merge pull request"*) && ($commitmessage == *"from petja-laitila/devel"*) ]]; then 
-  eval `ssh-agent -s`
-  ssh-add - <<< "${GA_DEPLOY_KEY}"
-  git config --global user.name "Github Actions"
-  git config --global user.email "github-actions[bot]@users.noreply.github.com"
-  # Use default merge strategy
-  git config --global pull.rebase false
-  # Push one branch at a time
-  git config --global push.default simple
-  
-  pushd .
-  mkdir ~/testing
-  cd ~/testing
-  git clone git@github.com:petja-laitila/actions_test.git
-  echo $PWD
-  cd actions_test
-  git checkout master 
-  git reset --hard
-  git pull
+eval `ssh-agent -s`
+ssh-add - <<< "${GA_DEPLOY_KEY}"
+git config --global user.name "Github Actions"
+git config --global user.email "github-actions[bot]@users.noreply.github.com"
+# Use default merge strategy
+git config --global pull.rebase false
+# Push one branch at a time
+git config --global push.default simple
+git pull  
+git checkout devel 
+git reset --hard
+git pull
 
-  echo $commitmessage | tee -a test
-  git add .
-  git commit -m "Updated some stuff to master"
-  git push
-  echo Merging changes back to devel
-  # git fetch
-  git checkout devel
-  git pull
-  git merge master
-  echo "Updated latest" | tee -a test2
-  git add .
-  git commit -m "Updated latest stuff to devel"
-  git pull
-  git push
-  popd
-  
-  #  --set-upstream origin devel
-fi;
+echo $commitmessage | tee -a test
+git add .
+git commit -m "Updated some stuff to devel"
+git push
+echo "Updated latest" | tee -a test2
+git add .
+git commit -m "Updated latest stuff to devel"
+git pull
+git push
